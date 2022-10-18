735e223fd219e54c917058c079fb680daae9920c
lack-of-gravitas <10094964+lack-of-gravitas@users.noreply.github.com>
2022-10-18 20:13:20 +1100

    update stripe

--- src/pages/api/stripe-webhooks.ts
+++ src/pages/api/stripe-webhooks.ts
@@ -2,11 +2,7 @@ import { stripe } from '@/lib/stripe'
 import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
 import { Readable } from 'node:stream'
-import {
-  upsertProductRecord,
-  upsertPriceRecord,
-  managePurchases,
-} from '@/lib/supabase-admin'
+import { postData } from '@/lib/api-helpers'
 
 // Stripe requires the raw body to construct the event.
 export const config = {
@@ -55,59 +51,102 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log('stripe triggered event --- ', event)
 
-    // if (relevantEvents.has(event.type)) {
-    //   try {
-    //     switch (event.type) {
-    //       case 'product.created':
-    //         // get directusid from metadata and update stripeId for product record in Supabase
-    //         await upsertProductRecord(event.data.object as Stripe.Product)
+    if (relevantEvents.has(event.type)) {
+      try {
+        // directus api - https://docs.directus.io/reference/items.html
+        let product
+        let price
+        let results: any
 
-    //       case 'product.updated':
-    //         await upsertProductRecord(event.data.object as Stripe.Product)
-    //         break
-    //       case 'price.created':
-    //       case 'price.updated':
-    //         await upsertPriceRecord(event.data.object as Stripe.Price)
-    //         break
-    //       case 'checkout.session.completed':
-    //         const checkoutSession = event.data.object as Stripe.Checkout.Session
-    //         await managePurchases(
-    //           // event.data.object as Stripe.Checkout.Session,
-    //           checkoutSession.mode === 'subscription'
-    //             ? (checkoutSession.subscription as string)
-    //             : checkoutSession.payment_intent,
-    //           checkoutSession.customer as string,
-    //           checkoutSession.metadata,
-    //           checkoutSession.mode as string
-    //         )
-    //         break
+        switch (event.type) {
+          case 'product.created':
+            product = event.data.object as Stripe.Product
+            results = await postData({
+              url: '/items/products',
+              data: {
+                id: product.id,
+                name: product.name,
+                description: product.description,
+                status: product.active === true ? 'active' : 'draft',
+              },
+              token: `${process.env.DIRECTUS}`,
+            })
 
-    //       // case 'customer.subscription.created':
-    //       // case 'customer.subscription.updated':
-    //       // case 'customer.subscription.deleted':
-    //       //   const subscription = event.data.object as Stripe.Subscription
-    //       //   await managePurchases(
-    //       //     // event.data.object
-    //       //     subscription.id,
-    //       //     subscription.customer as string,
-    //       //   )
-    //       //   break
+            console.log('directus product created results -- ', results)
+            break
+          case 'product.updated':
+            product = event.data.object as Stripe.Product
+            results = await postData({
+              url: `/items/products/` + product.id,
+              data: {
+                name: product.name,
+                description: product.description,
+                status: product.active === true ? 'active' : 'draft',
+              },
+              token: `${process.env.DIRECTUS}`,
+            })
 
-    //       default:
-    //         throw new Error('Unhandled relevant event!')
-    //     }
-    //   } catch (error) {
-    //     console.log(error)
-    //     return res
-    //       .status(400)
-    //       .send(
-    //         'Webhook error: "Webhook handler failed. View logs."' +
-    //           JSON.stringify(error)
-    //       )
-    //   }
-    // }
+            console.log('directus product created results -- ', results)
 
-    res.json({ received: true })
+            break
+          case 'price.created':
+            price = event.data.object as Stripe.Price
+            results = await postData({
+              url: '/items/price',
+              data: {
+                id: price.id,
+                currency: price.currency,
+                product: price.product,
+                unit_amount: ((price.unit_amount ?? 0) / 100).toFixed(2),
+              },
+              token: `${process.env.DIRECTUS}`,
+            })
+
+            console.log('directus price created results -- ', results)
+            break
+          case 'price.updated':
+            price = event.data.object as Stripe.Price
+            results = await postData({
+              url: `/items/products` + price.id,
+              data: {
+                currency: price.currency,
+                product: price.product,
+                unit_amount: ((price.unit_amount ?? 0) / 100).toFixed(2),
+              },
+              token: `${process.env.DIRECTUS}`,
+            })
+
+            console.log('directus price created results -- ', results)
+
+            break
+          // case 'checkout.session.completed':
+          //   const checkoutSession = event.data.object as Stripe.Checkout.Session
+          //   await managePurchases(
+          //     // event.data.object as Stripe.Checkout.Session,
+          //     checkoutSession.mode === 'subscription'
+          //       ? (checkoutSession.subscription as string)
+          //       : checkoutSession.payment_intent,
+          //     checkoutSession.customer as string,
+          //     checkoutSession.metadata,
+          //     checkoutSession.mode as string
+          //   )
+          //   break
+
+          // default:
+          //   throw new Error('Unhandled relevant event!')
+        }
+      } catch (error) {
+        console.log(error)
+        return res
+          .status(400)
+          .send(
+            'Webhook error: "Webhook handler failed. View logs."' +
+              JSON.stringify(error)
+          )
+      }
+    }
+
+    res.json({ received: true }) // acknowledge receipt of webhook and appropriate processing
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method Not Allowed')
@@ -115,163 +154,3 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 }
 
 export default webhookHandler
-
-// // price deleted
-
-// stripe triggered event ---  {
-//   id: 'evt_1Lt4weEvc4dteT8lr5EU7xFz',
-//   object: 'event',
-//   api_version: '2022-08-01',
-//   created: 1665819488,
-//   data: {
-//     object: {
-//       id: 'prod_MWetI5m6pENs5e',
-//       object: 'product',
-//       active: true,
-//       attributes: [],
-//       created: 1664514926,
-//       default_price: null,
-//       description: null,
-//       images: [],
-//       livemode: false,
-//       metadata: {},
-//       name: 'DEMO Thank You Card',
-//       package_dimensions: null,
-//       shippable: null,
-//       statement_descriptor: null,
-//       tax_code: 'txcd_99999999',
-//       type: 'service',
-//       unit_label: null,
-//       updated: 1665819488,
-//       url: null
-//     },
-//     previous_attributes: { updated: 1665819487 }
-//   },
-//   livemode: false,
-//   pending_webhooks: 1,
-//   request: {
-//     id: 'req_ynVUz8saN49EtZ',
-//     idempotency_key: '761de9ce-3a99-4eb7-93a0-cab3e8fb56dc'
-//   },
-//   type: 'product.updated'
-// }
-
-// // price created
-// stripe triggered event ---  {
-//   id: 'evt_1Lt4xaEvc4dteT8l5gVlzKBF',
-//   object: 'event',
-//   api_version: '2022-08-01',
-//   created: 1665819546,
-//   data: {
-//     object: {
-//       id: 'prod_MWetI5m6pENs5e',
-//       object: 'product',
-//       active: true,
-//       attributes: [],
-//       created: 1664514926,
-//       default_price: 'price_1Lt4xZEvc4dteT8lk9D7MZY8',
-//       description: null,
-//       images: [],
-//       livemode: false,
-//       metadata: {},
-//       name: 'DEMO Thank You Card',
-//       package_dimensions: null,
-//       shippable: null,
-//       statement_descriptor: null,
-//       tax_code: 'txcd_99999999',
-//       type: 'service',
-//       unit_label: null,
-//       updated: 1665819546,
-//       url: null
-//     },
-//     previous_attributes: { updated: 1665819545 }
-//   },
-//   livemode: false,
-//   pending_webhooks: 1,
-//   request: {
-//     id: 'req_9nEUHvS0KJa0XU',
-//     idempotency_key: '152c8c13-7cfc-46ec-8471-7a37df33328e'
-//   },
-//   type: 'product.updated'
-// }
-
-// // product updated
-// stripe triggered event ---  {
-//   id: 'evt_1Lt4yjEvc4dteT8lOT28a9qn',
-//   object: 'event',
-//   api_version: '2022-08-01',
-//   created: 1665819616,
-//   data: {
-//     object: {
-//       id: 'prod_MWetI5m6pENs5e',
-//       object: 'product',
-//       active: true,
-//       attributes: [],
-//       created: 1664514926,
-//       default_price: null,
-//       description: null,
-//       images: [],
-//       livemode: false,
-//       metadata: {},
-//       name: 'Thank You Card',
-//       package_dimensions: null,
-//       shippable: null,
-//       statement_descriptor: null,
-//       tax_code: 'txcd_99999999',
-//       type: 'service',
-//       unit_label: null,
-//       updated: 1665819616,
-//       url: null
-//     },
-//     previous_attributes: { name: 'DEMO Thank You Card', updated: 1665819577 }
-//   },
-//   livemode: false,
-//   pending_webhooks: 1,
-//   request: {
-//     id: 'req_cA11RdnXR9hIO2',
-//     idempotency_key: '00615750-c76b-4444-8f24-763bc3abc45c'
-//   },
-//   type: 'product.updated'
-// }
-
-// // customer created
-
-// stripe triggered event ---  {
-//   id: 'evt_1Lt4zWEvc4dteT8ld7OAIeYm',
-//   object: 'event',
-//   api_version: '2022-08-01',
-//   created: 1665819666,
-//   data: {
-//     object: {
-//       id: 'cus_McJcOuxkwEhUeT',
-//       object: 'customer',
-//       address: null,
-//       balance: 0,
-//       created: 1665819666,
-//       currency: null,
-//       default_source: null,
-//       delinquent: false,
-//       description: null,
-//       discount: null,
-//       email: 'info@tausigma.co',
-//       invoice_prefix: 'D064860E',
-//       invoice_settings: [Object],
-//       livemode: false,
-//       metadata: {},
-//       name: 'DEMO Customer',
-//       next_invoice_sequence: 1,
-//       phone: null,
-//       preferred_locales: [],
-//       shipping: null,
-//       tax_exempt: 'none',
-//       test_clock: null
-//     }
-//   },
-//   livemode: false,
-//   pending_webhooks: 1,
-//   request: {
-//     id: 'req_GXUG6yUfQXICbE',
-//     idempotency_key: '0d622f33-3546-4600-9ea7-2798814f26bc'
-//   },
-//   type: 'customer.created'
-// }
