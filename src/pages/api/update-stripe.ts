59b48be1c7b4f114ef5b2f4330ed78d470909f4c
lack-of-gravitas <10094964+lack-of-gravitas@users.noreply.github.com>
2022-10-18 14:38:02 +1100

    update product stripe

--- src/pages/api/update-stripe.ts
+++ src/pages/api/update-stripe.ts
@@ -5,35 +5,48 @@ import { stripe } from '@/lib/stripe'
 const updateStripe = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     console.log(JSON.stringify(req.body))
-    // const { event, payload, key, keys } = req.body
+    const { event, payload, key, keys } = req.body
 
-    // try {
-    //   switch (event) {
-    //     case 'items.create':
-    //         const product = await stripe.products.create ({
-    //             name: payload.name ?? 'NAME NOT SET',
-    //             active: payload.status === 'active' ? true: false,
-    //             description: payload.description ?? '',
-    //             images: [payload.images.create[0]]
-    //             metadata: key
-    //         })
-    //       break
-    //     case 'items.update':
-    //       break
-    //     case 'items.delete':
-    //       break
-    //     default:
-    //       throw new Error('Unhandled event. Check webhook.')
-    //   }
-    // } catch (error) {
-    //   console.log(error)
-    //   return res
-    //     .status(400)
-    //     .send(
-    //       'Webhook error: "Webhook handler failed. View logs."' +
-    //         JSON.stringify(error)
-    //     )
-    // }
+    try {
+      switch (event) {
+        case 'items.create':
+          let product = {
+            name: payload.name ?? 'NAME NOT SET',
+            active: payload.status === 'active' ? true : false,
+            description: payload.description ?? '',
+            metadata: key,
+            images: [
+              `${process.env.NEXT_PUBLIC_ASSETS_URL}/${payload.images.create[0].directus_files_id.id}`,
+            ],
+          }
+
+          payload.prices
+            ? (product = {
+                ...product,
+                ...{ default_price_data: [...payload.prices] },
+              })
+            : null
+
+          product = await stripe.products.create(product)
+
+          console.log('product -- ', product)
+          break
+        case 'items.update':
+          break
+        case 'items.delete':
+          break
+        default:
+          throw new Error('Unhandled event. Check webhook.')
+      }
+    } catch (error) {
+      console.log(error)
+      return res
+        .status(400)
+        .send(
+          'Webhook error: "Webhook handler failed. View logs."' +
+            JSON.stringify(error)
+        )
+    }
   }
 
   // 'prices' in JSON.parse(req.body) ? console.log('prices upserted') : null
@@ -45,24 +58,42 @@ export default updateStripe
 
 // create product
 // {
-//     event: 'items.create',
-//     accountability: {
-//       user: 'eee90e7e-69c1-4824-8dbd-5cee8f279ad3',
-//       role: 'bc0d61b1-ba8b-495f-a3fb-9db4c92b6668'
-//     },
-//     payload: {
-//       name: 'DEMO Stripe Product',
-//       type: 'card',
-//       images: { create: [Array], update: [], delete: [] },
-//       prices: [ [Object] ],
-//       description: 'test description',
-//       stockQty: 5,
-//       brand: 'Thankly',
-//       status: 'active'
-//     },
-//     key: 13,
-//     collection: 'products'
+//   "event": "items.create",
+//   "accountability": {
+//     "user": "eee90e7e-69c1-4824-8dbd-5cee8f279ad3",
+//     "role": "bc0d61b1-ba8b-495f-a3fb-9db4c92b6668"
+//   },
+//   "payload": {
+//     "name": "DEMO Product",
+//     "type": "card",
+// "prices": [
+//   {
+//     "currency": "aud",
+//     "unit_amount": 123
 //   }
+// ]
+//     "images": {
+//       "create": [
+//         {
+//           "products_id": "+",
+//           "directus_files_id": {
+//             "id": "1ec15772-2bcc-498f-a63f-a2dc7ca3a44e"
+//           }
+//         },
+//         {
+//           "products_id": "+",
+//           "directus_files_id": {
+//             "id": "ddc12764-9313-4f4c-a422-0de80fb90071"
+//           }
+//         }
+//       ],
+//       "update": [],
+//       "delete": []
+//     }
+//   },
+//   "key": 14,
+//   "collection": "products"
+// }
 
 // batch edit products (chaneg to gift)
 // {
